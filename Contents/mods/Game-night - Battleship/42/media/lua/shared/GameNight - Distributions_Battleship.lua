116ea6a4a1bee31c1071aca252f1f353f556c4f8
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-16 10:22:03 -0500

    Moved textures to /common for B42, and make B41 access B42 assets.

--- Contents/mods/Game-night - Battleship/42/media/lua/shared/GameNight - Distributions_Battleship.lua
+++ Contents/mods/Game-night - Battleship/42/media/lua/shared/GameNight - Distributions_Battleship.lua
@@ -3,12 +3,12 @@ require "Items/SuburbsDistributions"
 local gameNightDistro = require "gameNight - Distributions"
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["BattleShip_Box"] = {}
-gameNightDistro.gameNightBoxes["BattleShip_Box"] = { BattleShip_GameStand = 2, }
+gameNightDistro.gameNightBoxes["BattleShip_Box"] = {
+    BattleShip_GameStand = 2,
+    BattleShip_Ship_Battleship = 2, BattleShip_Ship_Carrier = 2, BattleShip_Ship_Destroyer = 2,
+    BattleShip_Ship_Submarine = 2, BattleShip_Ship_PatrolBoat = 2,
+    BattleShip_Peg_White = 168, BattleShip_Peg_Red = 84,
+}
 
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["BattleShip_GameStand"] = { chanceFactor = 0.0001,}
-gameNightDistro.gameNightBoxes["BattleShip_GameStand"] = {
-    BattleShip_Ship_Battleship = 1, BattleShip_Ship_Carrier = 1, BattleShip_Ship_Destroyer = 1,
-    BattleShip_Ship_Submarine = 1, BattleShip_Ship_PatrolBoat = 1,
-    BattleShip_Peg_White = 84, BattleShip_Peg_Red = 42,
-}
